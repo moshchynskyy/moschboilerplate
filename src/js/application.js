182b9184efe81d1eb5746e2c889d64133d3dcbf5
Anton Moshchynskyi <anton.moshchynskyi@nerdpeople.com>
2018-12-24 16:13:42 +0200

    first commit -- gulp is ready

--- src/js/application.js
+++ src/js/application.js
@@ -1,28 +1,36 @@
 ;'use strict';
 
-// slider
-$('#slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-});
-
-// wow js activate
-// new WOW().init();
-
 // preloader
 $(window).on('load', function () {
-    let $preloader = $('#p_prldr'),
-        $svg_anm = $preloader.find('.svg_anm');
+    let $preloader = $('#preloader'),
+        $svg_anm = $preloader.find('.preloader__svg');
     $svg_anm.fadeOut();
-    $preloader.delay(200).fadeOut('slow');
+    $preloader.fadeOut();
 });
 // eof preloader
 
+
+$(function () {
+
 // header
-$('#toggle-menu').on('click', function(e) {
-    e.preventDefault();
-    $(this).toggleClass("on");
-    $('.menu').addClass('menu-open').slideToggle();
-    // $("body").children().not('header').toggleClass('blur'); // add blur filter to all elements
-});
+    $('#toggle-menu').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass("on");
+        $('.menu').addClass('menu-open').slideToggle();
+        // $("body").children().not('header').toggleClass('blur'); // add blur filter to all elements
+    });
 // eof header
+
+// sliders
+    $('#slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+// eof sliders
+
+// WOW
+// new WOW().init();
+// eof WOW
+
+
+});
